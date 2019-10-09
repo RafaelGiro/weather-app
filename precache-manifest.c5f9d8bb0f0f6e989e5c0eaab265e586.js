0f7db5eb55a4b75bebfcd632ca78895394d36798
RafaelGiro <rafael.hyf@gmail.com>
2019-10-08 22:05:35 -0300

    Updates

---
+++ precache-manifest.c5f9d8bb0f0f6e989e5c0eaab265e586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff21880303c0de497da66c1e716560f6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ae7a1994c40f41888b38",
+    "url": "/weather-app/static/css/main.89944207.chunk.css"
+  },
+  {
+    "revision": "16aab6dd78e3b94450e3",
+    "url": "/weather-app/static/js/2.93c86c02.chunk.js"
+  },
+  {
+    "revision": "ae7a1994c40f41888b38",
+    "url": "/weather-app/static/js/main.2b523d6a.chunk.js"
+  },
+  {
+    "revision": "2a772a3f304d502ff61c",
+    "url": "/weather-app/static/js/runtime-main.4addd464.js"
+  }
+]);
